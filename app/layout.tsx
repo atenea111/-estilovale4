1b0c2881ca1b76cc6de612320f71d0984b98edab
atenea111 <ateneasoft1@gmail.com>
2025-07-31 15:47:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: 'EstiloVale4',
   description: 'Created with atenea',
   generator: 'atenea.dev',
+ icons: {
+    icon: '/images/logo.png',
+    shortcut: '/images/logo.png',
+    apple: '/images/logo.png',
+  },
 }
 
 export default function RootLayout({
